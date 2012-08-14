8a6797574159794dbafb9b1dc5b111e18f299074
Dave Doolin <david.doolin@gmail.com>
2012-08-14 11:20:59 -0700

    stubbed out command line processing

--- main.py
+++ main.py
@@ -4,8 +4,12 @@
 
 import argparse
 
-def foo():
-    print "foo"
+def parse_arguments():
+    parser = argparse.ArgumentParser(description='Process some integers.')
+    parser.add_argument('integers', metavar='N', type=int, nargs='+',
+                        help='an integer for the accumulator')
+    args = parser.parse_args()
+    print args.accumulate(args.integers)
 
 if __name__ == '__main__':
-    foo()
+    parse_arguments()
